753a60900985c9389833b526f93025614c3c6ef9
cristian.santamaria <cristian.santamaria@flecharoja.com>
2023-12-23 07:58:40 -0600

    mi primer commit

---
+++ src/app/componentes/imagen-central/imagen-central.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagen-central',
+  standalone: true,
+  imports: [],
+  templateUrl: './imagen-central.component.html',
+  styleUrl: './imagen-central.component.css'
+})
+export class ImagenCentralComponent {
+
+}
