922b313b3c86b9659260a86a459e0bfa5d5ae2be
cristian.santamaria <cristian.santamaria@flecharoja.com>
2023-12-25 18:03:32 -0600

    se inicia proyecto en version 15

--- dist/cjoesb/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-CIOJAL7I.mjs";import"./chunk-MXNWJSVT.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PiePaginaComponent } from "./componente/pie-pagina/pie-pagina.component";
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, PiePaginaComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'cjoesb';
