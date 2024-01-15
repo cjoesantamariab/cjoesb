9c359a9696460d811704147555bc9e1db5a20561
cristian.santamaria <cristian.santamaria@flecharoja.com>
2024-01-15 16:38:47 -0600

    clases para centrar

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -6,11 +6,13 @@ const InfoCard = () => {
     return (
         <div className="container">
             <div className="InfoCard">
-                <h2>Cristian Joe.. Santamaria</h2>
+                <h2 className="centrar1">Cristian Joe.. Santamaria</h2>
                 <div className="content"> 
                     <img src={horse} alt="Caballo de ajedrez" width="150" height="150" style={{ cursor: 'pointer' }}/>
                 </div>
-                <a href="https://www.linkedin.com/in/cristian-santamaria-870841134/"  target="_blank" rel="noopener noreferrer">Linkedin</a>
+                <div className="centrar1">
+                    <a href="https://www.linkedin.com/in/cristian-santamaria-870841134/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                </div>
             </div>
         </div>
     )
