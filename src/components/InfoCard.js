8ce33e1a1841cf6be27ae9db258d6ff1fa36c93d
cristian.santamaria <151423262+csnaith@users.noreply.github.com>
2024-01-27 16:38:56 -0600

    se actualizan iconos pendiente icono principal

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,17 +1,34 @@
 import React from "react";
 import './InfoCard.css';
-import horse from "../assets/svg/horse.svg";
+import logo from "../assets/svg/caas.svg";
 
 const InfoCard = () => {
+    const phoneNumber = "+50498582668";
+    const iconStyle = {
+        fontSize: '2rem', // Puedes ajustar el tamaño según tus preferencias
+        marginRight: '10px', // Espacio entre los iconos
+        color: 'black',
+    };
     return (
         <div className="container">
             <div className="InfoCard">
                 <h2 className="centrar1">Cristian Joel Santamaria</h2>
                 <div className="content"> 
-                    <img src={horse} alt="Caballo de ajedrez" width="150" height="150" style={{ cursor: 'pointer' }}/>
+                    <img src={logo} alt="log" style={{ cursor: 'pointer' }}/>
                 </div>
                 <div className="centrar1">
-                    <a href="https://www.linkedin.com/in/cristian-santamaria-870841134/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                    <a href="https://www.linkedin.com/in/cristian-santamaria-870841134/" target="_blank" rel="noopener noreferrer">
+                        <i className="pi pi-linkedin" style={iconStyle}></i>
+                    </a>
+                    <a href="https://www.instagram.com/cristianjoel7202/" target="_blank" rel="noopener noreferrer">
+                        <i className="pi pi-instagram" style={iconStyle}></i>
+                    </a>
+                    <a href="https://github.com/cjoesantamariab" target="_blank" rel="noopener noreferrer">
+                        <i className="pi pi-github" style={iconStyle}></i>
+                    </a>
+                    <a href={`https://web.whatsapp.com/send?phone=${encodeURIComponent(phoneNumber)}`} target="_blank" rel="noopener noreferrer">
+                        <i className="pi pi-whatsapp" style={iconStyle}></i>
+                    </a>
                 </div>
             </div>
         </div>
