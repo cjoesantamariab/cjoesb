036c1422c9d045d1ab1661a661689a04347d0f79
cristian.santamaria <151423262+csnaith@users.noreply.github.com>
2025-01-21 16:56:17 -0600

    se actualiza certificado aws solution architect

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,9 +13,10 @@ function AboutMe() {
     >
       <h2>Acerca de Mí</h2>
       <p>
-        Soy un desarrollador Full Stack con experiencia en múltiples tecnologías y plataformas. 
+        Soy un desarrollador Full Stack con experiencia en múltiples tecnologías y plataformas, incluyendo ASP.NET MVC, Angular, y AWS, con certificación como Arquitecto de Soluciones Asociado de AWS. He trabajado en el diseño, desarrollo e implementación de aplicaciones modernas, tanto a nivel de interfaz como en infraestructura en la nube. Mi objetivo es seguir creciendo profesionalmente y aportar soluciones innovadoras en proyectos desafiantes.
+        {/* Soy un desarrollador Full Stack con experiencia en múltiples tecnologías y plataformas. 
         He trabajado en proyectos usando ASP.NET MVC, Angular, AWS, y mucho más. 
-        Mi objetivo es seguir creciendo profesionalmente y contribuir a proyectos innovadores.
+        Mi objetivo es seguir creciendo profesionalmente y contribuir a proyectos innovadores. */}
       </p>
     </motion.section>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ function Header() {
       transition={{ duration: 0.8 }}
     >
       <h1>Cristian Santamaría</h1>
-      <h2>Desarrollador Full Stack</h2>
+      <h2>Ingeniero en Computación | Desarrollador Full Stack | Desarrollador en la Nube | Arquitecto de Soluciones Asociado de AWS</h2>
       <p>
-        Desarrollador apasionado con experiencia en soluciones en la nube y desarrollo de aplicaciones modernas.
+        Desarrollador apasionado con experiencia en soluciones en la nube, aplicaciones modernas y certificado como Arquitecto de Soluciones Asociado de AWS.
       </p>
       <div className="header-buttons">
         <a 
